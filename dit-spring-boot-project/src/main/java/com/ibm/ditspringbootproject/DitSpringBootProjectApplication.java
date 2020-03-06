d18aea1da96a420b50abd28bad14e1cbbf0c7b55
puneetvashisht@gmail.com <puneetvashsiht@gmail.com>
2020-03-06 10:05:55 +0530

    hg

---
+++ dit-spring-boot-project/src/main/java/com/ibm/ditspringbootproject/DitSpringBootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.ditspringbootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DitSpringBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DitSpringBootProjectApplication.class, args);
+	}
+
+}
---
+++ dit-spring-boot-project/src/test/java/com/ibm/ditspringbootproject/DitSpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.ibm.ditspringbootproject;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class DitSpringBootProjectApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
