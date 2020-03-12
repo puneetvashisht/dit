8954f46a278ea3977725e2320b908071c0cddd76
puneetvashisht@gmail.com <puneetvashsiht@gmail.com>
2020-03-12 15:04:00 +0530

    netflix eureka server

--- ibm-book-flight-service/src/main/java/com/ibm/ibmbookflightservice/IbmBookFlightServiceApplication.java
+++ ibm-book-flight-service/src/main/java/com/ibm/ibmbookflightservice/IbmBookFlightServiceApplication.java
@@ -2,8 +2,10 @@ package com.ibm.ibmbookflightservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableFeignClients("com.ibm.ibmbookflightservice")
 public class IbmBookFlightServiceApplication {
 
