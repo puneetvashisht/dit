ca8935e55c3e861b95f1f8e7f57b2ce324f601a8
puneetvashisht@gmail.com <puneetvashsiht@gmail.com>
2020-04-07 12:17:12 +0530

    opr

--- frontend/01basics/functionsdemo.js
+++ frontend/01basics/functionsdemo.js
@@ -32,7 +32,7 @@ function outer() {
 }
 // var x = outer();
 // x();
-outer()();
+// outer()();
 
 // 1. Creation of lexical scopes
 // outerFn: [x: undefined, inner: undefined]
@@ -44,3 +44,18 @@ outer()();
 
 var obj = {}
 console.log(typeof(obj))
+
+var employee = {id:12, name:'Ravi', salary: 34343.34, getName: function(){return this.name}}
+console.log(employee);
+console.log(employee.getName());
+employee.city = "Delhi";
+
+var address1 = {houseNumber: 134, locality: 'GK', city: 'Delhi'}
+var address2 = {houseNumber: 133, locality: 'GK', city: 'Delhi'}
+
+var addresses = []
+addresses.push(address1)
+addresses.push(address2)
+addresses.push(employee);
+console.log(addresses)
+// console.log(employee['name']);
