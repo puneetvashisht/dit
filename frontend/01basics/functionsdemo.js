2103195f26c513a1a619596be3f3a7cbbc2dd369
puneetvashisht@gmail.com <puneetvashsiht@gmail.com>
2020-04-07 11:03:24 +0530

    kg

--- frontend/01basics/functionsdemo.js
+++ frontend/01basics/functionsdemo.js
@@ -1,8 +1,8 @@
-function foo(){
+function foo() {
     console.log('inside foo function');
 }
 
-var bar = function(){
+var bar = function () {
     console.log('inside bar function');
 }
 
@@ -10,12 +10,37 @@ var bar = function(){
 // bar();
 
 
-function printMe(fn){
- console.log('Inside printMe function')
- fn();
+function printMe(fn) {
+    console.log('Inside printMe function')
+    fn();
 }
 
-printMe(foo);
-printMe(bar);
-printMe(function(){console.log('inside test funtion')})
-printMe(()=>console.log('inside yet another function'))
+// printMe(foo);
+// printMe(bar);
+// printMe(function(){console.log('inside test funtion')})
+// printMe(()=>console.log('inside yet another function'))
+
+function outer() {
+    var x = 2;
+    console.log('outer function..')
+    var inner = function () {
+        console.log("inner function..")
+        console.log(x+2);
+        var x = 0;
+    }
+    return inner;
+}
+// var x = outer();
+// x();
+outer()();
+
+// 1. Creation of lexical scopes
+// outerFn: [x: undefined, inner: undefined]
+// innerFn: [x: undefined]
+
+// 2. Execution
+// outerFn: [x: 2, inner: fn]
+// innerFn: [x: undefined]
+
+var obj = {}
+console.log(typeof(obj))
