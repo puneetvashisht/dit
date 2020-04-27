714da4ac58c81996ec7140e16ed665d9b9e06b2b
puneetvashisht@gmail.com <puneetvashsiht@gmail.com>
2020-04-27 15:28:25 +0530

    state and props

--- frontend/my-react-app/src/Dropdown.js
+++ frontend/my-react-app/src/Dropdown.js
@@ -7,28 +7,37 @@ class Dropdown extends Component {
 
     constructor(props){
         super(props);
-        this.state = {courses : ['Angular', 'React', 'Ember']}
+        this.state = {courses : ['Angular', 'React', 'Ember'], showFlag: false, caption: null}
     }
 
     handleClick(){
         console.log('click event on dropdown button')
+        console.log(this.state)
+        // this.state.showFlag = !this.state.showFlag
+        this.setState({showFlag: !this.state.showFlag})
+    }
+
+    handleItemClick(item){
+        console.log('Item is getting clicked..', item)
+        // this.state.caption = item
+        this.setState({caption: item, showFlag: false})
     }
 
     render() {
 
         let courseList = this.state.courses.map((course, i)=> {
             console.log(course, i)
-            return <a className="dropdown-item" href="#">{course}</a>
+            return <a className="dropdown-item" onClick={this.handleItemClick.bind(this, course)} key={i} href="#">{course}</a>
         })
         console.log(courseList)
 
 
         return (
             <div className="dropdown">
-                <button onClick={this.handleClick} className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    {this.props.caption}
+                <button onClick={this.handleClick.bind(this)} className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    {this.state.caption || this.props.caption} 
                 </button>
-                <div className="dropdown-menushow" aria-labelledby="dropdownMenuButton">
+                <div className={this.state.showFlag?'dropdown-menushow': 'dropdown-menu'} aria-labelledby="dropdownMenuButton">
                 {courseList}
                 </div>
             
